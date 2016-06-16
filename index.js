0a303a25f46de8c3b62fbf7c292c3f1d8645f408
Massimiliano Marcon <me@marcon.me>
2016-06-16 23:06:24 +0200

    Prettifies code

--- index.js
+++ index.js
@@ -14,28 +14,28 @@ const tomsUrl = url.resolve(config[country].baseUrl, slug);
 
 debug(`Requesting ${tomsUrl}...`);
 
-request(tomsUrl, function(error, response, body){
-	if(error) {
-		return console.error(error);
-	}
-	
-	debug(`Parsing page`);
-	
-	const $ = cheerio.load(body);
-	const sizeEntry = $('select#product-size option').map(function(){
-		return $(this).text().trim();
-	})
-	.get()
-	.filter((s) => s.split(' ')[0] === size)
-	.shift();
+request(tomsUrl, function(error, response, body) {
+    if (error) {
+        return console.error(error);
+    }
 
-	if(!sizeEntry) {
-		return console.log(`${chalk.bgYellow(' ')} ${chalk.yellow('Size not found')}`);
-	}
+    debug(`Parsing page`);
 
-	if(/out of stock/i.test(sizeEntry)) {
-		console.log(`${chalk.bgRed(' ')} ${chalk.red('Out of stock')}`);
-	} else {
-		console.log(`${chalk.bgGreen(' ')} ${chalk.green('Available')}`);
-	}
+    const $ = cheerio.load(body);
+    const sizeEntry = $('select#product-size option').map(function() {
+            return $(this).text().trim();
+        })
+        .get()
+        .filter((s) => s.split(' ')[0] === size)
+        .shift();
+
+    if (!sizeEntry) {
+        return console.log(`${chalk.bgYellow(' ')} ${chalk.yellow('Size not found')}`);
+    }
+
+    if (/out of stock/i.test(sizeEntry)) {
+        console.log(`${chalk.bgRed(' ')} ${chalk.red('Out of stock')}`);
+    } else {
+        console.log(`${chalk.bgGreen(' ')} ${chalk.green('Available')}`);
+    }
 });
